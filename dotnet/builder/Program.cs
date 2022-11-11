e2748c3c68f0d1cdf38e91b548cbb4e2a06d6f22
dzgntutar <duzguntutar@gmail.com>
2022-11-11 23:01:25 +0300

    c sharp builder

--- dotnet/builder/Program.cs
+++ dotnet/builder/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using builder;
+
+var qParam = new Dictionary<string, string>();
+qParam["name"] = "Kaan";
+qParam["age"] = "30";
+
+var url = new UrlBuilder("https://abc.com")
+    .AddParameter("api")
+    .AddParameter("v1")
+    .AddParameter("customer")
+    .AddQueryParam(qParam)
+    .Build();
+
+Console.WriteLine(url);
---
+++ dotnet/builder/UrlBuilder.cs
@@ -0,0 +1,56 @@
+using System.Text;
+
+namespace builder
+{
+    public class UrlBuilder
+    {
+        public StringBuilder sbUrl { get; set; }
+        public string BaseUrl { get; set; }
+        private const string _Delimeter = "/";
+
+        public UrlBuilder(string BaseUrl)
+        {
+            sbUrl = new StringBuilder();
+            this.BaseUrl = BaseUrl;
+        }
+
+        public UrlBuilder AddParameter(string parameter)
+        {
+            if (!string.IsNullOrEmpty(parameter))
+            {
+                sbUrl.AppendFormat("{0}{1}", _Delimeter, parameter);
+            }
+
+            return this;
+        }
+
+        public UrlBuilder AddQueryParam(Dictionary<string, string> qParams)
+        {
+            if (qParams.Keys.Any())
+                sbUrl.Append("?");
+
+            foreach (var param in qParams)
+            {
+                sbUrl.Append(String.Format("{0}={1}&", param.Key, param.Value));
+            }
+
+            return this;
+        }
+
+        public string Build()
+        {
+            var qparameter = sbUrl.ToString();
+
+            System.Console.WriteLine(qparameter);
+
+            if (qparameter[^1] == '&')
+                qparameter = qparameter[0..(qparameter.Length - 1)];
+
+            System.Console.WriteLine(qparameter);
+
+            var url = $"{this.BaseUrl}{qparameter}";
+
+            return url;
+        }
+    }
+}
